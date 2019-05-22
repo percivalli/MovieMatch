806f3539d61ecb57168b5434e9f13a4626ce53bb
percivalli <thamuz.khaos@gmail.com>
2019-05-22 16:36:58 -0300

    Java básico.

--- app/src/main/java/br/com/digitalhouse/moviematch/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/moviematch/MainActivity.java
@@ -2,12 +2,33 @@ package br.com.digitalhouse.moviematch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCriar;
+    Button btnFacebook;
+    Button btnGoogle;
+    Switch btnTermos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        btnCriar = findViewById(R.id.btnCriar);
+        btnFacebook = findViewById(R.id.btnFacebook);
+        btnGoogle = findViewById(R.id.btnGoogle);
+        btnTermos = findViewById(R.id.btnTermos);
+
+        btnCriar.setOnClickListener(this);
+        btnFacebook.setOnClickListener(this);
+        btnGoogle.setOnClickListener(this);
+        btnTermos.setOnClickListener(this);
     }
+
+    public void telaCriar(View v) {}
 }
