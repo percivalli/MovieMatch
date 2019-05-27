b209f61c1fe4bf38ffc97b3ddca454547465315c
Taddeus10 <leandro.taddeu@gmail.com>
2019-05-27 00:57:13 -0300

    Criação da classe cadastro ainda incompleta precisando validar algumas informações

--- app/src/main/java/br/com/digitalhouse/moviematch/Cadastro/Cadastro.java
+++ app/src/main/java/br/com/digitalhouse/moviematch/Cadastro/Cadastro.java
@@ -1,4 +1,68 @@
 package br.com.digitalhouse.moviematch.Cadastro;
 
 public class Cadastro {
+
+    private String nome;
+    private int idade;
+    private String sexo;
+    private String cidade;
+    private String email;
+    private String senha;
+
+    public Cadastro(String nome, int idade, String sexo, String cidade, String email, String senha) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.cidade = cidade;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
