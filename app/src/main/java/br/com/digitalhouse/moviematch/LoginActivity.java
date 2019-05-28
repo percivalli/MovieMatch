e86fca30ec8c3b7321b5fff858f20a390029a265
gustavoshitara <gus_port@yahoo.com.br>
2019-05-28 20:05:57 -0300

    Junção do XML com o Java da Tela de Criar Conta

--- app/src/main/java/br/com/digitalhouse/moviematch/LoginActivity.java
+++ app/src/main/java/br/com/digitalhouse/moviematch/LoginActivity.java
@@ -20,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
 
         initViews();
