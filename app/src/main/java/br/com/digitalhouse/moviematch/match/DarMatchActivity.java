367a7a78ea0a73f05d0d84edd2890206028e51aa
Mrodrigues123 <murillozba@hotmail.com>
2019-06-07 18:59:19 -0300

    Alterações na tela de "Match"

---
+++ app/src/main/java/br/com/digitalhouse/moviematch/match/DarMatchActivity.java
@@ -0,0 +1,15 @@
+package br.com.digitalhouse.moviematch.match;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.digitalhouse.moviematch.R;
+
+public class DarMatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dar_match);
+    }
+}
