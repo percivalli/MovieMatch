4d2cae202e8545e81557c38365183ea004fce528
alexanmm <alexandre.moreira.medeiros@gmail.com>
2019-06-12 13:52:29 -0300

    Ajustes Tela Home Login

--- app/src/main/java/br/com/digitalhouse/moviematch/home/HomeActivity.java
+++ app/src/main/java/br/com/digitalhouse/moviematch/home/HomeActivity.java
@@ -1,12 +1,11 @@
 package br.com.digitalhouse.moviematch.home;
 
 import android.content.Intent;
-import android.support.design.widget.TextInputLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 import android.widget.Toast;
 
 import br.com.digitalhouse.moviematch.R;
@@ -17,15 +16,16 @@ public class HomeActivity extends AppCompatActivity {
     private Button btnHomeCriar;
     private Button btnHomeFacebook;
     private Button btnHomeGoogle;
-    private Switch swithHomeTermos;
+    private SwitchCompat swithHomeTermos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
         //Incialização das Views
-        inicializaDadosHome();
+        inicializaViews();
 
         //Botão Criar Conta
         btnHomeCriar.setOnClickListener(new View.OnClickListener() {
@@ -69,16 +69,6 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
-    public void inicializaDadosHome() {
-
-        //Incialização das Views
-        btnHomeCriar = findViewById(R.id.btnHomeCriar);
-        btnHomeFacebook = findViewById(R.id.btnHomeFacebook);
-        btnHomeGoogle = findViewById(R.id.btnHomeGoogle);
-        swithHomeTermos = findViewById(R.id.swithHomeTermos);
-
-    }
-
     public boolean validaTermoUso() {
 
         if (swithHomeTermos.isChecked()) { //True
@@ -93,5 +83,15 @@ public class HomeActivity extends AppCompatActivity {
 
             return false;
         }
+    }
+
+    //Incialização das Views
+    public void inicializaViews(){
+
+        btnHomeCriar = findViewById(R.id.btnHomeCriar);
+        btnHomeFacebook = findViewById(R.id.btnHomeFacebook);
+        btnHomeGoogle = findViewById(R.id.btnHomeGoogle);
+        swithHomeTermos = findViewById(R.id.swithHomeTermos);
+
     }
 }
--- app/src/main/java/br/com/digitalhouse/moviematch/splash/SplashActivity.java
+++ app/src/main/java/br/com/digitalhouse/moviematch/splash/SplashActivity.java
@@ -11,6 +11,7 @@ import java.util.TimerTask;
 
 import br.com.digitalhouse.moviematch.R;
 import br.com.digitalhouse.moviematch.home.HomeActivity;
+import br.com.digitalhouse.moviematch.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
