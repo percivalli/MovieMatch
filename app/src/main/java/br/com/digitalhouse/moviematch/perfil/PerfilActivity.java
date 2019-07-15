013707e68103039b0bab072e3dcf913b38bafb08
martinsfelipemartins <martins.felipe1@hotmail.com>
2019-07-15 18:25:42 -0300

    debito do espaçamento de background (aumentando background)

--- app/src/main/java/br/com/digitalhouse/moviematch/perfil/PerfilActivity.java
+++ app/src/main/java/br/com/digitalhouse/moviematch/perfil/PerfilActivity.java
@@ -2,9 +2,7 @@ package br.com.digitalhouse.moviematch.perfil;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,6 +11,9 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -130,7 +131,6 @@ public class PerfilActivity extends AppCompatActivity implements AdapterView.OnI
 
         editTextPerfilNome = findViewById(R.id.editTextPerfilNome);
         editTextPerfilIdade = findViewById(R.id.editTextPerfilIdade);
-        editTextPerfilSexo = findViewById(R.id.editTextPerfilSexo);
         spinnerPerfilCidade = findViewById(R.id.spinnerPerfilCidade);
         spinnerPerfilInteresse = findViewById(R.id.spinnerPerfilInteresse);
         textViewPerfilMeusMatchs = findViewById(R.id.textViewPerfilMeusMatchs);
