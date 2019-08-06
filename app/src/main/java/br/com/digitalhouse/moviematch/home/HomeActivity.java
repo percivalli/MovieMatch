94ae3779604bfab3411c81d05ca64571a426b0f7
percivalli <thamuz.khaos@gmail.com>
2019-08-06 15:19:03 -0300

    Tentativa Login Google.

--- app/src/main/java/br/com/digitalhouse/moviematch/home/HomeActivity.java
+++ app/src/main/java/br/com/digitalhouse/moviematch/home/HomeActivity.java
@@ -6,23 +6,45 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import br.com.digitalhouse.moviematch.R;
 import br.com.digitalhouse.moviematch.login.LoginActivity;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private Button btnHomeCriar;
     //private Button btnHomeFacebook;
     private Button btnHomeGoogle;
     private SwitchCompat swithHomeTermos;
+    private GoogleSignInClient googleSignInClient;
+    private FirebaseAuth firebaseAuth;
+
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    private static final int RESULT_GOOGLE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         //Incialização das Views
         inicializaViews();
@@ -43,17 +65,17 @@ public class HomeActivity extends AppCompatActivity {
         });
 
         //Botão Usar Facebook
-       // btnHomeFacebook.setOnClickListener(new View.OnClickListener() {
+        // btnHomeFacebook.setOnClickListener(new View.OnClickListener() {
         //    @Override
         //    public void onClick(View v) {
 
-                //Permite entrar pela conta Facebook somente se o termo de uso for aceito
+        //Permite entrar pela conta Facebook somente se o termo de uso for aceito
         //        if (validaTermoUso()) {
-                    // --> Incluir chamada da Tela para Login pelo Facebook
+        // --> Incluir chamada da Tela para Login pelo Facebook
 //                }
 //
-  //          }
-    //    });
+        //          }
+        //    });
 
         //Botão Usar Google
         btnHomeGoogle.setOnClickListener(new View.OnClickListener() {
@@ -63,10 +85,95 @@ public class HomeActivity extends AppCompatActivity {
                 //Permite entrar pela conta Google somente se o termo de uso for aceito
                 if (validaTermoUso()) {
                     // --> Incluir chamada da Tela para Login pelo Google
+                    GoogleSignInOptions options = new GoogleSignInOptions
+                            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                            .requestIdToken(getString(R.string.default_web_client_id))
+                            .requestEmail()
+                            .build();
+
+                    googleSignInClient = GoogleSignIn.getClient(HomeActivity.this, options);
+                    signIn();
                 }
 
             }
         });
+
+    }
+
+    //Incialização das Views
+    public void inicializaViews() {
+
+        btnHomeCriar = findViewById(R.id.btnHomeCriar);
+        //  btnHomeFacebook = findViewById(R.id.btnHomeFacebook);
+        btnHomeGoogle = findViewById(R.id.btnHomeGoogle);
+        swithHomeTermos = findViewById(R.id.swithHomeTermos);
+
+    }
+
+    private void signIn() {
+        Intent intent = googleSignInClient.getSignInIntent();
+        startActivityForResult(intent, RESULT_GOOGLE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_GOOGLE) {
+            // The Task returned from this call is always completed, no need to attach a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                authWithGoogle(account);
+
+            } catch (ApiException e) {
+
+                e.printStackTrace();
+
+            }
+        }
+    }
+
+    private void authWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        goToHome();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Auth Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void goToHome() {
+        // Sign in success, update UI with the signed-in user's information
+        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+        finish();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(getApplicationContext(), "Authentication Error", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (authStateListener != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 
     public boolean validaTermoUso() {
@@ -83,15 +190,5 @@ public class HomeActivity extends AppCompatActivity {
 
             return false;
         }
-    }
-
-    //Incialização das Views
-    public void inicializaViews() {
-
-        btnHomeCriar = findViewById(R.id.btnHomeCriar);
-    //  btnHomeFacebook = findViewById(R.id.btnHomeFacebook);
-        btnHomeGoogle = findViewById(R.id.btnHomeGoogle);
-        swithHomeTermos = findViewById(R.id.swithHomeTermos);
-
     }
 }
--- app/src/main/java/br/com/digitalhouse/moviematch/login/LoginActivity.java
+++ app/src/main/java/br/com/digitalhouse/moviematch/login/LoginActivity.java
@@ -3,38 +3,34 @@ package br.com.digitalhouse.moviematch.login;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import br.com.digitalhouse.moviematch.R;
-import br.com.digitalhouse.moviematch.cadastro.CadastroUsuarioActivity;
-import br.com.digitalhouse.moviematch.perfil.PerfilActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
+
+import br.com.digitalhouse.moviematch.R;
+import br.com.digitalhouse.moviematch.cadastro.CadastroUsuarioActivity;
+import br.com.digitalhouse.moviematch.home.HomeActivity;
+import br.com.digitalhouse.moviematch.perfil.PerfilActivity;
 
 //import android.support.v7.app.AppCompatActivity;
 //import androidx.appcompat.widget.Toolbar;
 
-public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class LoginActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private TextView toobarTitle;
@@ -46,71 +42,76 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     //  private Button btnLoginFacebook;
     private Button btnLoginGoogle;
     private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth.AuthStateListener authStateListener;
     private GoogleSignInClient googleSignInClient;
     private static final int RESULT_GOOGLE = 200;
-    private FirebaseAuth.AuthStateListener authStateListener;
+    //private CallbackManager callbackManager;
 
+    public LoginActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_login );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
         // Toolbar
         //toolbar = findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
-        toobarTitle = findViewById( R.id.toolbarTitleSimples );
-        toobarTitle.setText( "CRIAR SUA CONTA" );
+        toobarTitle = findViewById(R.id.toolbarTitleSimples);
+        toobarTitle.setText("CRIAR SUA CONTA");
         firebaseAuth = FirebaseAuth.getInstance();
 
 
         //Incialização das Views
-        textInputLayoutLoginEmail = findViewById( R.id.textInputLayoutLoginEmail );
-        textInputLayoutLoginPassword = findViewById( R.id.textInputLayoutLoginPassword );
-        btnLoginLogin = findViewById( R.id.btnLoginLogin );
-        btnLoginCriarConta = findViewById( R.id.btnLoginCriarConta );
+        textInputLayoutLoginEmail = findViewById(R.id.textInputLayoutLoginEmail);
+        textInputLayoutLoginPassword = findViewById(R.id.textInputLayoutLoginPassword);
+        btnLoginLogin = findViewById(R.id.btnLoginLogin);
+        btnLoginCriarConta = findViewById(R.id.btnLoginCriarConta);
         //    btnLoginFacebook = findViewById(R.id.btnLoginFacebook);
-        btnLoginGoogle = findViewById( R.id.btnLoginGoogle );
+        btnLoginGoogle = findViewById(R.id.btnLoginGoogle);
 
         authStateListener = firebaseAuth -> {
             FirebaseUser user = firebaseAuth.getCurrentUser();
             if (user != null) {
-                startActivity( new Intent( getApplicationContext(), PerfilActivity.class ) );
+                startActivity(new Intent(getApplicationContext(), PerfilActivity.class));
                 finish();
             }
+
         };
 
         //Botão Login
-        btnLoginLogin.setOnClickListener( new View.OnClickListener() {
+        btnLoginLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 if (validaDadosLogin()) {
 
                     //Chama a tela de Perfil
-                    Intent intent = new Intent( LoginActivity.this,
-                            PerfilActivity.class );
+                    Intent intent = new Intent(LoginActivity.this,
+                            PerfilActivity.class);
 
                     /*Intent intent = new Intent(LoginActivity.this,
                             FavoritosActivity.class);*/
 
-                    startActivity( intent );
+                    startActivity(intent);
                 }
             }
-        } );
+        });
 
         //Botão Criar Conta
-        btnLoginCriarConta.setOnClickListener( new View.OnClickListener() {
+        btnLoginCriarConta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent( LoginActivity.this,
-                        CadastroUsuarioActivity.class );
+                Intent intent = new Intent(LoginActivity.this,
+                        CadastroUsuarioActivity.class);
 
-                startActivity( intent );
+                startActivity(intent);
             }
-        } );
+        });
 
         //Botão User Facebook
         //       btnLoginFacebook.setOnClickListener(new View.OnClickListener() {
@@ -123,22 +124,17 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         //  });
 
         //Botão Usar Google
-        btnLoginGoogle.setOnClickListener( v -> {
-            try {
-                //Google Sign In
-                GoogleSignInOptions options = new GoogleSignInOptions
-                        .Builder( GoogleSignInOptions.DEFAULT_SIGN_IN )
-                        .requestIdToken( getString( R.string.default_web_client_id ) )
-                        .requestEmail()
-                        .build();
+        btnLoginGoogle.setOnClickListener(v -> {
+            //Google Sign In
+            GoogleSignInOptions options = new GoogleSignInOptions
+                    .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build();
 
-                googleSignInClient = GoogleSignIn.getClient( this, options );
-                signIn();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } );
+        });
     }
+
 
     private boolean validaDadosLogin() {
         {
@@ -150,103 +146,37 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             int minimalPassLen = 6;
 
             //Inicializa o set Error
-            textInputLayoutLoginEmail.setError( "" );
-            textInputLayoutLoginPassword.setError( "" );
+            textInputLayoutLoginEmail.setError("");
+            textInputLayoutLoginPassword.setError("");
 
             //Email Obrigatório
             if (textEmail.isEmpty()) {
-                textInputLayoutLoginEmail.setError( "Favor preencher o Email" );
+                textInputLayoutLoginEmail.setError("Favor preencher o Email");
                 return false;
             }
 
             //Senha Obrigatória
             if (textPassword.isEmpty()) {
-                textInputLayoutLoginPassword.setError( "Favor preencher a Senha" );
+                textInputLayoutLoginPassword.setError("Favor preencher a Senha");
                 return false;
             }
 
             if (textPassword.length() < minimalPassLen) {
-                textInputLayoutLoginPassword.setError( "Digite uma senha com 6 ou mais caracteres" );
+                textInputLayoutLoginPassword.setError("Digite uma senha com 6 ou mais caracteres");
                 return false;
             }
 
             if (!(textEmail.isEmpty()) && !(textPassword.isEmpty())) {
 
                 //Grava as Preferencias do Usuario
-                SharedPreferences preferences = getSharedPreferences( "APP", MODE_PRIVATE );
+                SharedPreferences preferences = getSharedPreferences("APP", MODE_PRIVATE);
 
-                preferences.edit().putString( "EMAIL", textEmail ).commit();
-                preferences.edit().putString( "SENHA", textPassword ).commit();
+                preferences.edit().putString("EMAIL", textEmail).commit();
+                preferences.edit().putString("SENHA", textPassword).commit();
 
                 return true;
             }
             return true;
         }
-    }
-
-    private void signIn() {
-        Intent intent = googleSignInClient.getSignInIntent();
-        startActivityForResult( intent, RESULT_GOOGLE );
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult( requestCode, resultCode, data );
-
-        if (requestCode == RESULT_GOOGLE) {
-            // The Task returned from this call is always completed, no need to attach a listener.
-            Task <GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent( data );
-
-            try {
-
-                GoogleSignInAccount account = task.getResult( ApiException.class );
-                authWithGoogle( account );
-
-            } catch (ApiException e) {
-
-                e.printStackTrace();
-
-            }
-        }
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Toast.makeText( getApplicationContext(), "Authentication Error", Toast.LENGTH_SHORT ).show();
-    }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        firebaseAuth.addAuthStateListener( authStateListener );
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (authStateListener != null) {
-            firebaseAuth.removeAuthStateListener( authStateListener );
-        }
-    }
-
-    private void authWithGoogle(GoogleSignInAccount account) {
-        AuthCredential credential = GoogleAuthProvider.getCredential( account.getIdToken(), null );
-
-        firebaseAuth.signInWithCredential( credential )
-                .addOnCompleteListener( this, task -> {
-                    if (task.isSuccessful()) {
-                        goToHome();
-                    } else {
-                        Toast.makeText( getApplicationContext(), "Auth Error", Toast.LENGTH_SHORT ).show();
-                    }
-                } );
-    }
-
-    private void goToHome() {
-        // Sign in success, update UI with the signed-in user's information
-        startActivity( new Intent( getApplicationContext(), PerfilActivity.class ) );
-        finish();
     }
 }
